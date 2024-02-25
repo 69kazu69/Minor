90ece928c60503772dc50c31aac3ebd4af4d91b9
Aanchal  Walia <aanchalwalia@Aanchals-MacBook-Air.local>
2024-02-25 13:24:25 +0530

    added api in the funtioning

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 from Blockchain import Blockchain
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/sample', methods=['POST'])
 def main() -> None:
     obj = Blockchain('Genesis Block')
-    
-    while True:
-        data=input('Enter Data: ')
-        os.system('clear')
-
-        if data=='':
-            print(obj.show())
-            obj.makeFile()
-            response_data = {'message': 'Form submitted successfully'}
-            return jsonify(response_data)
-            
-        else:
-            obj.allHash()
-            obj.addBlock(data)
-            
-            os.system('sleep 0.1')
-
+    json_data = request.json
+    data = json_data.get('data')
+    for i in data:
+        obj.addBlock(i)
+    print(obj.show())
+    obj.makeFile()
+    response_data = {'message': 'Form submitted successfully'}
+    return jsonify(response_data)
 if __name__ == '__main__':
     app.run(debug=True)
